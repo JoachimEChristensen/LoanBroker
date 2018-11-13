3a124e4286a23501e0224a72f72859fd1f1f4681
Christian Philip Ege Nielsen <kagejohn@kagejohn.dk>
2018-11-13 18:39:48 +0100

    CreditScore is more or less done
    It only needs the part that send the number to the next component

--- CreditScore/Program.cs
+++ CreditScore/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CreditScore
@@ -10,6 +11,63 @@ namespace CreditScore
     {
         static void Main(string[] args)
         {
+            CreditScoreService creditScoreService = new CreditScoreService();
+            string ssn = Ssn();
+
+            creditScoreService.creditScoreAsync(ssn);
+
+            creditScoreService.creditScoreCompleted += CreditScoreCompleted;
+
+            Console.WriteLine("Waiting for credit score.");
+            Console.ReadLine();
+        }
+
+        private static void CreditScoreCompleted(object sender, creditScoreCompletedEventArgs e)
+        {
+            int creditScore = e.Result;
+
+            Console.WriteLine("Credit score: " + creditScore);
+        }
+
+        private static string Ssn()
+        {
+            string ssn = "";
+
+            Console.WriteLine("Input SSN:");
+
+            Match match = Regex.Match(ssn, @"^\d{6}-\d{4}$");
+            bool enter = false;
+            bool firstTry = true;
+
+            while (!match.Success && !enter)
+            {
+                if (!firstTry)
+                {
+                    Console.WriteLine("Try again: (XXXXXX-XXXX)");
+                }
+                ssn = Console.ReadLine();
+                match = Regex.Match(ssn, @"^\d{6}-\d{4}$");
+
+                if (firstTry)
+                {
+                    firstTry = false;
+                }
+                if (ssn == "")
+                {
+                    enter = true;
+                }
+            }
+
+            if (ssn == "")
+            {
+                Random random = new Random();
+                string randomBeginning = random.Next(1, 999999).ToString("000000");
+                string randomEnding = random.Next(1, 9999).ToString("0000");
+                ssn = randomBeginning + "-" + randomEnding;
+            }
+
+            Console.WriteLine("SSN: " + ssn);
+            return ssn;
         }
     }
 }
