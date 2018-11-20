a4fd7b077572e907b4c5e2cacbceed9af547a203
Joachim E Christensen <JoachimEChristensen@gmail.com>
2018-11-20 19:22:21 +0100

    GetBanks koncept komplet, mangler rulebase implementation

--- Banks/Program.cs
+++ Banks/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Banks.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,11 @@ namespace Banks
         static void Main(string[] args)
         {
             //get.content();
-            //rulebase(content);
+
+            List<Bank> banks = new List<Bank>();
+
+            banks.add(rulebase(creditScore, loanAmount));
+
             //send();
         }
     }
